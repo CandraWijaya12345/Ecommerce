66d918e7d486baf05ee7ba44ef14c5dbef965aab
candrawijaya06 <hacciwijaya06@gmail.com>
2023-05-29 23:49:17 +0800

    Menambahkan data pada database dan menambah GET users

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,21 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 6092), 0);
-
-        httpServer.createContext("/user", new UserHandler()); // Inisialisasi dengan UserHandler
-
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.start();
-    }
-
-    public static class UserHandler implements HttpHandler {
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            if ("GET".equals((exchange.getRequestMethod()))) {
-                String responseToBeSentBack = SQLkoneksi.Connection();
-                exchange.sendResponseHeaders(200, responseToBeSentBack.length());
-                OutputStream outputStream = exchange.getResponseBody();
-                outputStream.write(responseToBeSentBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
-            }
+        try{
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8092),0);
+            httpserver.createContext("/", new Server());
+            httpserver.setExecutor(Executors.newSingleThreadExecutor());
+            httpserver.start();
+            System.out.println("Listening on port 8092...");
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
